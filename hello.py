4fd5fc8bbcfeef85f3e5d2c9da90a3904f5ef815
charismafight@hotmail.com <charismafight@hotmail.com>
2018-08-05 23:32:23 +0800

    simple start

--- hello.py
+++ hello.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "hello flask"
+def index():
+    return "this is the index page of app"
+
+@app.route("/hello")
+def Hello():
+    return "you come to hello page now"
