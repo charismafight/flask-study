04160f9faa4bd3c36b9ae2fee6c74101a9fd7448
charismafight@hotmail.com <charismafight@hotmail.com>
2018-08-17 23:58:44 +0800

    url parameter

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "this is the index page of app"
 
+
 @app.route("/hello")
-def Hello():
+def hello():
     return "you come to hello page now"
+
+
+@app.route("/user/<userid>")
+def show_user(userid):
+    return 'this is userid: %s' % userid
